1adde84a3d8c6e919793307bc4496d1cfdbec600
laurent.gateau <defwxyz@free.fr>
2021-04-25 15:05:45 +0200

    add maxsize parameter

--- src/haskell/random-picture/app/Main.hs
+++ src/haskell/random-picture/app/Main.hs
@@ -14,16 +14,21 @@ import LibRender (renderAlea, quote)
 -- given a geometry (width, height) 
 -- make a svg header string
 svgHeader :: Geometry -> String
-svgHeader (Geometry screenWidth screenHeight) = "<svg version=\"1.1\" width=" ++ (quote screenWidth) ++ " height=" ++ (quote screenHeight) ++ " xmlns=\"http://www.w3.org/2000/svg\">\n"
+svgHeader (Geometry screenWidth screenHeight) = 
+    "<svg version=\"1.1\" width=" 
+    ++ (quote screenWidth) 
+    ++ " height=" 
+    ++ (quote screenHeight) 
+    ++ " xmlns=\"http://www.w3.org/2000/svg\">\n"
 
 svgFooter = "</svg>"
 
 -- given a geometry and a number of figures
 -- make the given number of svg figures 
 -- with random integers
-svgBody :: Geometry -> Int -> IO String
-svgBody geometry howMany = do
-   a <- (replicateM howMany (randomFigure (Geometry 1920 1080) 30 ))
+svgBody :: Geometry -> Int -> Int -> IO String
+svgBody geometry howMany maxsize = do
+   a <- (replicateM howMany (randomFigure geometry maxsize))
    lignes <- (mapM renderAlea a)
    return (unlines lignes)
  
@@ -32,16 +37,18 @@ program = do
     args <- getArgs
     let g = (read (args !! 0)) :: Geometry
     let nb = (read (args !! 1)) :: Int 
-    content <- svgBody g nb 
+    let maxsize = (read (args !! 2)) :: Int 
+    content <- svgBody g nb maxsize 
     let svgScript = (svgHeader g) ++ content ++ svgFooter
     putStrLn svgScript 
 
 usage = "Usage:\n\
-        \  random-picture-exe geometry nb\n\
+        \  random-picture-exe geometry nb maxsize\n\
         \  geometry must follow width x height format\n\
         \  nb is the number of shapes\n\n\
+        \  maxsize is the maximum size of shapes\n\n\
         \Example:\n\
-        \  random-picture-exe 640x480 4000\n"
+        \  random-picture-exe 640x480 400 10\n"
 
 main = catch (program) handler
     where 
