f066d6a5f08442d911eba427faf25076928dd83a
defwxyz <defwxyz@free.fr>
2021-05-03 10:49:05 +0200

    use Int64 instead of Integer

--- src/haskell/jiffy/app/Main.hs
+++ src/haskell/jiffy/app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Int
 import System.IO
 import Data.Time.Clock.System
 import Control.Monad
@@ -9,11 +10,12 @@ import Text.Printf
 import Data.Maybe
 
 data Measure = Measure {
-   start :: Integer,
-   laps :: Integer
+   start :: Int64,
+   laps :: Int64
 } deriving (Eq, Show)
 
 
+stillSameSecond :: Int64 -> Int64 -> IO Bool
 stillSameSecond start _ = do
   now <- liftM systemSeconds getSystemTime
   return (start /= now)
@@ -21,17 +23,17 @@ stillSameSecond start _ = do
 jiffy _ = do 
     startSecond <- liftM systemSeconds getSystemTime 
     result <- firstM (stillSameSecond startSecond) [1..]
-    let out = (Measure (fromIntegral startSecond) (fromJust result))
+    let out = (Measure startSecond (fromJust result))
     putStrLn (show out)
     return out
 
 
-bogoMips :: [ Measure ] -> Double
-bogoMips measures = (mSum / mN) / 500 where
-    mSum = fromIntegral (sum (map laps measures))
+bogoMips :: [ Int64 ] -> Double
+bogoMips measures = mSum / (mN * 500.0) where
+    mSum = fromIntegral (sum measures)
     mN = fromIntegral (length measures)
 
 main = do
    jiffy 0  -- calibrate
    r <- mapM jiffy [1..20]
-   printf "ok - %.2f BogoMIPS\n" (bogoMips r) 
+   printf "ok - %.2f BogoMIPS\n" (bogoMips (map laps r)) 
